4c447fb1a50c62805e937399191b29729ea3c6f8
tomfriedhof <tomfriedhof@62769.no-reply.drupal.org>
2012-06-05 15:33:37 +0200

    Issue #1615368 by tomfriedhof: Solved the issue related to work with https.

--- templates/fb_likebox.tpl.php
+++ templates/fb_likebox.tpl.php
@@ -7,7 +7,7 @@
 ?>
 
 <iframe
-src="http://www.facebook.com/plugins/likebox.php?href=<?php print $fb_url; ?>&width=<?php print $fb_width; ?>&colorscheme=<?php print $fb_colorscheme; ?>&show_faces=<?php print $fb_show_faces; ?>&bordercolor&stream=<?php echo $fb_stream; ?>&header=<?php print $fb_header; ?>&height=<?php print $fb_height; ?>"
+src="//www.facebook.com/plugins/likebox.php?href=<?php print $fb_url; ?>&width=<?php print $fb_width; ?>&colorscheme=<?php print $fb_colorscheme; ?>&show_faces=<?php print $fb_show_faces; ?>&bordercolor&stream=<?php echo $fb_stream; ?>&header=<?php print $fb_header; ?>&height=<?php print $fb_height; ?>"
 scrolling="<?php echo $fb_scrolling; ?>"
 frameborder="0"
 style="border: none; overflow: hidden; width: <?php print $fb_width; ?>px; height: <?php print $fb_height; ?>px;"
