6cff08393c9ab7333a826f5bf7228c98f13d008f
David Rozas <david.rozas@gmail.com>
2012-10-25 12:57:51 +0100

    Replacing & due to Issue #1818770

--- templates/fb_likebox.tpl.php
+++ templates/fb_likebox.tpl.php
@@ -7,7 +7,7 @@
 ?>
 
 <iframe
-src="//www.facebook.com/plugins/likebox.php?href=<?php print $fb_url; ?>&width=<?php print $fb_width; ?>&colorscheme=<?php print $fb_colorscheme; ?>&show_faces=<?php print $fb_show_faces; ?>&bordercolor&stream=<?php echo $fb_stream; ?>&header=<?php print $fb_header; ?>&height=<?php print $fb_height; ?>"
+src="//www.facebook.com/plugins/likebox.php?href=<?php print $fb_url; ?>&amp;width=<?php print $fb_width; ?>&amp;colorscheme=<?php print $fb_colorscheme; ?>&amp;show_faces=<?php print $fb_show_faces; ?>&amp;bordercolor&stream=<?php echo $fb_stream; ?>&amp;header=<?php print $fb_header; ?>&amp;height=<?php print $fb_height; ?>"
 scrolling="<?php echo $fb_scrolling; ?>"
 frameborder="0"
 style="border: none; overflow: hidden; width: <?php print $fb_width; ?>px; height: <?php print $fb_height; ?>px;"
