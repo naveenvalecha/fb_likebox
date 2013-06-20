73e1f4504c5e2fd1156e80423e4bf9270af3c13b
drozas <david.rozas@gmail.com>
2013-06-20 17:01:50 +0100

    Issue #2018691 : Including the new attributes: show_border and force_wall

--- templates/fb_likebox.tpl.php
+++ templates/fb_likebox.tpl.php
@@ -7,9 +7,9 @@
 ?>
 
 <iframe
-src="//www.facebook.com/plugins/likebox.php?href=<?php print $fb_url; ?>&amp;width=<?php print $fb_width; ?>&amp;colorscheme=<?php print $fb_colorscheme; ?>&amp;show_faces=<?php print $fb_show_faces; ?>&amp;bordercolor&stream=<?php echo $fb_stream; ?>&amp;header=<?php print $fb_header; ?>&amp;height=<?php print $fb_height; ?>"
+src="//www.facebook.com/plugins/likebox.php?href=<?php echo $fb_url; ?>&width=<?php echo $fb_width; ?>&colorscheme=<?php echo $fb_colorscheme; ?>&show_faces=<?php echo $fb_show_faces; ?>&bordercolor&stream=<?php echo $fb_stream; ?>&header=<?php echo $fb_header; ?>&height=<?php echo $fb_height; ?>&show_border=<?php echo $fb_show_border; ?>&force_wall=<?php echo $fb_force_wall; ?>"
 scrolling="<?php echo $fb_scrolling; ?>"
 frameborder="0"
-style="border: none; overflow: hidden; width: <?php print $fb_width; ?>px; height: <?php print $fb_height; ?>px;"
+style="border: none; overflow: hidden; width: <?php echo $fb_width; ?>px; height: <?php echo $fb_height; ?>px;"
 allowTransparency="true">
 </iframe>
