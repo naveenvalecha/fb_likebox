168df8035ab30bc93fdc883d115b49a885c3f484
David Rozas <david.rozas@gmail.com>
2013-08-17 14:04:34 +0100

    Issue #1668806 by drozas: Updating settings and blockForm() to use the new config system

--- lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
+++ lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
@@ -26,8 +26,19 @@ class FBLikeboxBlock extends BlockBase {
    */
   public function settings() {
     return array(
-        'seconds_online' => 900,
-        'max_list_count' => 10
+      'properties' => array(
+        'administrative' => TRUE
+      ),
+      'fb_likebox_url' => 'http://www.facebook.com/wikisaber.pt',
+      'fb_likebox_colorscheme' => 'light',
+      'fb_likebox_header' => 'true',
+      'fb_likebox_stream' => 'true',
+      'fb_likebox_show_faces' => 'true',
+      'fb_likebox_scrolling' => 'no',
+      'fb_likebox_force_wall' => 'false',
+      'fb_likebox_width' => 292,
+      'fb_likebox_height' => 556,
+      'fb_likebox_show_border' => 'true',
     );
   }
   /**
@@ -49,14 +60,14 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_url'] = array(
       '#type' => 'textfield',
       '#title' => t('Facebook Page URL'),
-      '#default_value' => variable_get('fb_likebox_url', 'http://www.facebook.com/wikisaber.es'),
+      '#default_value' => $this->configuration['fb_likebox_url'],
       '#description' => t('Enter the Facebook Page URL. I.e.: http://www.facebook.com/wikisaber.es'),
       '#required' => TRUE,
     );
     $form['fb_likebox_display_settings']['fb_likebox_colorscheme'] = array(
       '#type' => 'select',
       '#title' => t('Color Scheme'),
-      '#default_value' => variable_get('fb_likebox_colorscheme', 'light'),
+      '#default_value' => $this->configuration['fb_likebox_colorscheme'],
       '#options' => array(
         'light' => t('Light'),
         'dark' => t('Dark'),
@@ -67,7 +78,7 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_header'] = array(
       '#type' => 'select',
       '#title' => t('Header'),
-      '#default_value' => variable_get('fb_likebox_header', 'true'),
+      '#default_value' => $this->configuration['fb_likebox_header'],
       '#options' => array(
         'false' => t('No'),
         'true' => t('Yes'),
@@ -78,7 +89,7 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_stream'] = array(
       '#type' => 'select',
       '#title' => t('Stream'),
-      '#default_value' => variable_get('fb_likebox_stream', 'true'),
+      '#default_value' => $this->configuration['fb_likebox_stream'],
       '#options' => array(
         'false' => t('No'),
         'true' => t('Yes'),
@@ -89,7 +100,7 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_show_faces'] = array(
       '#type' => 'select',
       '#title' => t('Show Faces'),
-      '#default_value' => variable_get('fb_likebox_show_faces', 'true'),
+      '#default_value' => $this->configuration['fb_likebox_show_faces'],
       '#options' => array(
         'false' => t('No'),
         'true' => t('Yes'),
@@ -100,7 +111,7 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_scrolling'] = array(
       '#type' => 'select',
       '#title' => t('Scrolling'),
-      '#default_value' => variable_get('fb_likebox_scrolling', 'no'),
+      '#default_value' => $this->configuration['fb_likebox_scrolling'],
       '#options' => array(
         'no' => t('Disabled'),
         'yes' => t('Enabled'),
@@ -111,7 +122,7 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_display_settings']['fb_likebox_force_wall'] = array(
         '#type' => 'select',
         '#title' => t('Force wall'),
-        '#default_value' => variable_get('fb_likebox_force_wall', 'false'),
+        '#default_value' => $this->configuration['fb_likebox_force_wall'],
         '#options' => array(
             'false' => t('No'),
             'true' => t('Yes'),
@@ -123,21 +134,21 @@ class FBLikeboxBlock extends BlockBase {
     $form['fb_likebox_theming_settings']['fb_likebox_width'] = array(
       '#type' => 'textfield',
       '#title' => t('Width'),
-      '#default_value' => variable_get('fb_likebox_width', '292'),
+      '#default_value' => $this->configuration['fb_likebox_width'],
       '#description' => t('The width of the Facebook likebox in pixels.'),
       '#required' => TRUE,
     );
     $form['fb_likebox_theming_settings']['fb_likebox_height'] = array(
       '#type' => 'textfield',
       '#title' => t('Height'),
-      '#default_value' => variable_get('fb_likebox_height', '556'),
+      '#default_value' => $this->configuration['fb_likebox_height'],
       '#description' => t('The height of the plugin in pixels. The default height provided by Facebook API varies based on number of faces to display, and whether the stream is displayed. With the stream displayed, and 10 faces the default height is 556px. With no faces, and no stream the default height is 63px. You will need to play with these value if you haved disabled those features and you want the block to be displayed without an empty section.'),
       '#required' => TRUE,
     );
     $form['fb_likebox_theming_settings']['fb_likebox_show_border'] = array(
         '#type' => 'select',
         '#title' => t('Border'),
-        '#default_value' => variable_get('fb_likebox_show_border', 'true'),
+        '#default_value' => $this->configuration['fb_likebox_show_border'],
         '#options' => array(
             'false' => t('No'),
             'true' => t('Yes'),
