e5dd0327597099a77ce31c42c12fd8dbc82e3653
David Rozas <david.rozas@gmail.com>
2013-08-17 13:48:27 +0100

    Solving issue with namespace

--- lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
+++ lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @file
- * Contains \Drupal\user\Plugin\Block\FBLikeboxBlock.
+ * Contains \Drupal\fb_likebox\Plugin\Block\FBLikeboxBlock.
  */
 
-namespace Drupal\user\Plugin\Block;
+namespace Drupal\fb_likebox\Plugin\Block;
 
 use Drupal\block\BlockBase;
 use Drupal\Component\Annotation\Plugin;
@@ -19,19 +19,57 @@ use Drupal\Core\Annotation\Translation;
  *   module = "fb_likebox"
  * )
  */
-class UserOnlineBlock extends BlockBase {
+class FBLikeboxBlock extends BlockBase {
 
   /**
    * Overrides \Drupal\block\BlockBase::settings().
    */
   public function settings() {
     return array(
-        'properties' => array(
-            'administrative' => TRUE
-        ),
         'seconds_online' => 900,
         'max_list_count' => 10
     );
+  }
+  /**
+   * Overrides \Drupal\block\BlockBase::blockForm().
+   */
+  public function blockForm($form, &$form_state) {
+    $period = drupal_map_assoc(array(30, 60, 120, 180, 300, 600, 900, 1800, 2700, 3600, 5400, 7200, 10800, 21600, 43200, 86400), 'format_interval');
+    $form['user_block_seconds_online'] = array(
+        '#type' => 'select',
+        '#title' => t('User activity'),
+        '#default_value' => $this->configuration['seconds_online'],
+        '#options' => $period,
+        '#description' => t('A user is considered online for this long after they have last viewed a page.')
+    );
+    $form['user_block_max_list_count'] = array(
+        '#type' => 'select',
+        '#title' => t('User list length'),
+        '#default_value' => $this->configuration['max_list_count'],
+        '#options' => drupal_map_assoc(array(0, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100)),
+        '#description' => t('Maximum number of currently online users to display.')
+    );
+    return $form;
+  }
+  
+  /**
+   * Overrides \Drupal\block\BlockBase::blockSubmit().
+   */
+  public function blockSubmit($form, &$form_state) {
+    $this->configuration['seconds_online'] = $form_state['values']['user_block_seconds_online'];
+    $this->configuration['max_list_count'] = $form_state['values']['user_block_max_list_count'];
+  }
+  
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = array(
+        //'#theme' => 'item_list__user__online',
+        '#prefix' => '<p> This is just a test</p>',
+    );
+    return $build;
   }
 }
 ?>
