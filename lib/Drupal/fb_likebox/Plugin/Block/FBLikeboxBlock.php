e800c9b662f36e0ff76d5212e9234759413cfce7
drozas <david.rozas@gmail.com>
2013-08-22 14:42:11 +0100

    Cleaning up and adding to-dos

--- lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
+++ lib/Drupal/fb_likebox/Plugin/Block/FBLikeboxBlock.php
@@ -194,8 +194,6 @@ class FBLikeboxBlock extends BlockBase {
     $this->configuration['fb_likebox_width'] = $form_state['values']['fb_likebox_theming_settings']['fb_likebox_width'];
     $this->configuration['fb_likebox_height'] = $form_state['values']['fb_likebox_theming_settings']['fb_likebox_height'];
     $this->configuration['fb_likebox_show_border'] = $form_state['values']['fb_likebox_theming_settings']['fb_likebox_show_border'];
-    //@TO-DO: Figure out if we should set the config values here as well.
-    //config('fb_likebox')->set('fb_likebox_url', $form_state['values']['fb_likebox_display_settings']['fb_likebox_url'])->save(); 
   }
   
   /**
